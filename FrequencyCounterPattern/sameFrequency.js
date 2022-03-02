be4a1fde008c1a3978df40935c74e987c0dbd8ec
Dileep <98578797+iamdileep89@users.noreply.github.com>
2022-03-02 19:10:01 +0000

    more problems

---
+++ FrequencyCounterPattern/sameFrequency.js
@@ -0,0 +1,30 @@
+// Find out if the 2 numbers have same frequency of digits
+
+function sameFrequency(val1, val2){
+    val1 = val1.toString();
+    val2 = val2.toString();
+
+    if(val1.length != val2.length) return false
+
+    let frequencyVal1 = {}
+    let frequencyVal2 = {}
+
+    for(let i=0; i<val1.length; i++){
+        frequencyVal1[val1[i]] = (frequencyVal1[val1[i]] || 0)+1
+    }
+
+    for(let i=0; i<val2.length; i++){
+        frequencyVal2[val2[i]] = (frequencyVal2[val2[i]] || 0)+1
+    }
+
+    for(let key in frequencyVal1){
+        if(!(key in frequencyVal2)) return false
+        if(!(frequencyVal1[key] == frequencyVal2[key])){
+            return false
+        }
+    }
+    return true
+}
+
+sameFrequency(182,281)
+sameFrequency(182458,281854)
---
+++ areThereDuplicates.js
@@ -0,0 +1,44 @@
+//check if the arguments consists of duplicates
+
+// frequency counter
+function areThereDuplicates(...args){
+    let collection = {}
+    for(let key in args){
+        collection[key] = (collection[key]||0)+1
+    }
+
+    for(let key in collection){
+        if(collection[key]>1){
+            return true
+        }
+    }
+    return false
+}
+
+// multiple  Pointers
+function areThereDuplicates(...args){
+    //sort the array first
+    args.sort((a,b) => a-b)
+
+    let start = 0;
+    let next = 1
+
+    while(next<arr.length){
+        if(args[start]==args[next]){
+            return true
+        }
+        start++
+        next++
+    }
+    return false
+}
+
+//one liner solutoon
+
+function areThereDuplicates(){
+    return new Set(arguments).size !== arguments.length)
+}
+
+
+areThereDuplicates(a,b,c) // false
+areThereDuplicates(1, 3, 2, 3) // true
